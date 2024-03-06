b8f0d6e75999e6d2e2b2bd5436cba82d3f755552
mathixu <mathieuroux71@gmail.com>
2024-03-06 17:05:14 +0100

    Style application and ensure responsiveness

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,11 @@
 import { Routes } from '@angular/router';
-import {AppComponent} from "./app.component";
 import {EventCreationPageComponent} from "./pages/event-creation-page/event-creation-page.component";
 import {EventsListPageComponent} from "./pages/events-list-page/events-list-page.component";
 import {EventEditionPageComponent} from "./pages/event-edition-page/event-edition-page.component";
 
 export const routes: Routes = [
-  { path: '', component: AppComponent },
   { path: 'events', component: EventsListPageComponent },
   { path: 'events/add', component: EventCreationPageComponent },
   { path: 'events/edit/:id', component: EventEditionPageComponent },
+  { path: '**', redirectTo: '/events' },
 ];
